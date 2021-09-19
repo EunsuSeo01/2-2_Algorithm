cec4cefa631e8dd3623b20383f18c20648fc1b23
EunsuSeo01 <eunsu0613@naver.com>
2021-09-19 14:48:48 +0900

    end

--- homework_3.cpp
+++ homework_3.cpp
@@ -9,54 +9,64 @@
 #include <stdlib.h>
 #include <string.h>
 
-int subsequenceCount(char S[], char T[])
+/*
+ [subsequenceCount]
+ description: Return the number of sub-sequence in sequence string.
+ input: X -> Sequence string. Z -> sub-sequence string.
+ output: The number of sub-sequence in sequence string
+*/
+int subsequenceCount(char X[], char Z[])
 {
-	int m = strlen(T);
-	int n = strlen(S);
+	int m = strlen(Z);
+	int n = strlen(X);
 
-	if (m > n) return 0;
+	if (m > n) return 0;	// Invaild.
 
+	// Create Matrix.
 	int** mat = (int**)malloc(sizeof(int*) * (m + 1));
-	for (int i = 0; i < m; i++) {
+	for (int i = 0; i <= m; i++) {
 		mat[i] = (int*)malloc(sizeof(int) * (n + 1));
 	}
 
 	// Initializing first column with all 0s.
-	for (int i = 0; i < m; i++) {
+	for (int i = 0; i <= m; i++) {
 		mat[i][0] = 0;
 	}
 
 	// Initializing first row with all 1s.
-	for (int i = 0; i < n; i++) {
-		mat[0][i] = 1;
+	for (int j = 0; j <= n; j++) {
+		mat[0][j] = 1;
 	}
 
 	// Fill mat[][] in bottom up manner
 	for (int i = 1; i <= m; i++) {
 		for (int j = 1; j <= n; j++) {
-			
+			if (X[j - 1] == Z[i- 1])
+				mat[i][j] = mat[i][j - 1] + mat[i - 1][j - 1];
+			else
+				mat[i][j] = mat[i][j - 1];
 		}
 	}
 
 	// return value
-	/* code here */
+	return mat[m][n];
 }
 
 int main()
 {
 	int n;
-	char T[10000];
-	char S[100];
+	char X[10000];
+	char Z[100];
 
 	printf("Type the number of test sets: ");
 	scanf("%d", &n);
 
 	for (int i = 0; i < n; i++) {
 		printf("Type sequence: ");
-		gets_s(S);
+		scanf("%s", X);
 		printf("Type sub-sequence: ");
-		gets_s(T);
-		printf("Result: %d \n", subsequenceCount(S, T));
+		scanf("%s", Z);
+		printf("Result: %d \n", subsequenceCount(X, Z));
 	}
 
 	return 0;
