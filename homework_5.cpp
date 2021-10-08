0a52cae099ecb07b1b8a56f869c9c1ca4fdeca8e
EunsuSeo01 <eunsu0613@naver.com>
2021-10-09 01:46:54 +0900

    HW5 initial commit

---
+++ homework_5.cpp
@@ -0,0 +1,20 @@
+/*
+ 202035340 ¼­Àº¼ö.
+ Algorithm Homework 05 : Contest Trip.
+
+ <My strategy>
+ 1. Read a file.
+ 2. 
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX_CITY 100
+#define MAX_TRAIN 100
+
+#include <stdio.h>
+
+int main() {
+
+
+	return 0;
+}
