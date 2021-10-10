e0f4677915b0ae46c4ece8f655728f344cde9d31
EunsuSeo01 <eunsu0613@naver.com>
2021-10-11 00:13:39 +0900

    implement end

--- homework_5.cpp
+++ homework_5.cpp
@@ -3,8 +3,10 @@
  Algorithm Homework 05 : Contest Trip.
 
  <My strategy>
- 1. Read a file.
- 2. Use struct array.
+ 1. 출발지부터 목적지까지 가는 가능한 경로를 다 찾는다.
+ 2. 그 중 목적지에 도착한 시간이 최소인 것을 고른다.
+ 2-1. 하나라면 그것이 정답.
+ 2-2. 여러개면 그 중 출발 시간이 최대인 것을 고른다.
 */
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -15,11 +17,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-typedef struct timetable {
-	int timestamp;
-	char city[20];
-} timeT;
 
 void make_timetable(int n, int** timeT, char** city, int cityN) {
 	int temp_time = 0;
@@ -34,20 +31,35 @@ void make_timetable(int n, int** timeT, char** city, int cityN) {
 	}
 }
 
-void find_shortest(int** timeT, int col, int row) {
-	int starting_time = 0;
-	char src[NAME_SIZE], dest[NAME_SIZE];
-
-	scanf("%d", &starting_time);
-	scanf("%s", src);
-	scanf("%s", dest);
+// 출발지부터 목적지까지 가는 가능한 경로를 다 찾는다.
+void find_available_path(int** timeT, char** city, int row, int col, int starting_time, char* src, char* dest) {
+	int src_index = -1;
+	int dest_index = -1;
+	int start = 0, end = 0;
 
 	for (int j = 0; j < col; j++) {
 		for (int i = 0; i < row; i++) {
-
+			if (strcmp(city[i], src) == 0) {
+				src_index = i;
+				dest_index = j;
+				start = timeT[i][j];
+				break;
+			}
 		}
 	}
-	
+	printf("%04d %s\n", start, src);
+
+
+	for (int i = src_index; i < row; i++) {
+		if (timeT[i][dest_index] != 0 && end == 0) {
+			//if (strcmp(city[i], dest) != 0)
+
+			end = timeT[i][dest_index];
+		}
+	}
+	printf("%04d %s\n", start, src);
+
+	printf("%04d %s\n", end, dest);
 }
 
 int main() {
@@ -57,7 +69,7 @@ int main() {
 	
 	for (int i = 0; i < scenario; i++) {
 		scanf("%d\n", &city_num);
-		
+
 		// Defensive coding.
 		if (city_num < 1 || city_num > MAX_CITY) {
 			exit(0);
@@ -70,12 +82,13 @@ int main() {
 		}										// 연결된 도시의 수까지 받음.
 
 		scanf("%d", &table_num);
+
 		int** timetable = (int**)calloc(sizeof(int), city_num);
 		for (int i = 0; i < table_num; i++) {
 			timetable[i] = (int*)calloc(sizeof(int), table_num);
 		}
 
-		for(int i = 0; i < table_num; i++) {
+		for (int i = 0; i < table_num; i++) {
 			int iter = 0;
 			scanf("%d", &iter);
 
@@ -83,9 +96,18 @@ int main() {
 				make_timetable(i, timetable, city, city_num);
 			}
 		}	// 시간표 까지 다 받음.
-		
-		find_shortest(timetable, table_num, city_num);
+
+		int starting_time = 0;
+		char src[NAME_SIZE], dest[NAME_SIZE];
+
+		scanf("%d", &starting_time);
+		scanf("%s", src);
+		scanf("%s", dest);
+
+		for (int i = 0; i < city_num; i++) {
+			find_available_path(timetable, city, city_num, table_num, starting_time, src, dest);
+		}
 	}
-  
+
 	return 0;
 }
