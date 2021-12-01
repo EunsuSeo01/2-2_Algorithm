4e349dc00d75412782617ce5d76f7ea3157bb995
EunsuSeo01 <eunsu0613@naver.com>
2021-12-02 03:32:00 +0900

    Implement find_starting_point function

--- Clean_Campus_ver1.c
+++ Clean_Campus_ver1.c
@@ -3,15 +3,16 @@
  Problem 14: Clean Campus - Using Graham's scan algorithm.
  1:22~
 */
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct point {
-	int x;
-	int y;
+	float x;
+	float y;
 } P;
 
-P find_starting_point(P* point);
+P find_starting_point(P* point, int point_num);
 void sort_point(P criterion);
 P* graham_scan(P* point);
 
@@ -20,13 +21,30 @@ int main() {
 	scanf("%d", &point_num);
 
 	P* point = (P*)malloc(sizeof(P) * point_num);
-	sort_point(find_starting_point(point));
+
+	for (int i = 0; i < point_num; i++) {
+		scanf("%f %f", &point[i].x, &point[i].y);
+	}
+	sort_point(find_starting_point(point, point_num));
 
 	return 0;
 }
 
-P find_starting_point(P* point) {
+P find_starting_point(P* point, int point_num) {
 	/* y값 제일 작은 point 찾기 */
+	P min = point[0];
+	for (int i = 1; i < point_num; i++) {
+		if (min.y == point[i].y) {
+			if (min.x >= point[i].x) {
+				min = point[i];
+			}
+		}
+		else if (min.y > point[i].y) {
+			min = point[i];
+		}
+	}
+
+	return min;
 }
 
 void sort_point(P criterion) {
